9ebe30c67608af8b116fa1702ef741c659a7bc89
Elyse <elyse-groenendaal@outlook.com>
2022-03-01 15:18:25 +0900

    js for-loop

---
+++ loops.js
@@ -0,0 +1,10 @@
+// for-loop: how did it work again in JS? 
+
+const character = 't';
+let myString = '';
+
+for (let i = 0; i < 5; i++) {
+  myString = myString + character;
+}
+
+console.log(myString);
