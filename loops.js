3b665f7807e6d0944f3c7fe522637672b0422fd5
Elyse <elyse-groenendaal@outlook.com>
2022-03-04 13:34:56 +0900

    added sort method and forEach loop

--- loops.js
+++ loops.js
@@ -1,7 +1,7 @@
-// for-loop: how did it work again in JS? 
+// for-loop: how did it work again in JS?
 
-const character = 't';
-let myString = '';
+const character = "t";
+let myString = "";
 
 for (let i = 0; i < 5; i++) {
   myString = myString + character;
@@ -12,10 +12,10 @@ console.log(myString);
 // scope
 
 // any variable that is declared inside a block, like a loop, if-statement etc,
-// is going out of scope when the block finishes. 
+// is going out of scope when the block finishes.
 // You can't read the value of that variable
 
-// so it matters a lot where the variable is created. 
+// so it matters a lot where the variable is created.
 
 const A = "A";
 let F;
@@ -46,3 +46,28 @@ doStuff("B");
 console.log(B); // parameters destroyed after functioning is over, won't work
 console.log(C); // out of scope
 console.log(F);
+
+// you can pass around functions like variables
+
+// forEach
+
+cities.forEach(function (city) {
+  console.log(city);
+});
+
+// sort method: sort string to uppercase
+
+const strings = ["a", "c", "B", "d", "e"];
+
+strings
+  .map(function (string) {
+    return string.toUpperCase();
+  })
+  .sort();
+
+// one line
+
+strings
+  .map((string) => string.toUpperCase())
+  .sort()
+  .filter((s) => s !== "I"); // filters out what returns false
