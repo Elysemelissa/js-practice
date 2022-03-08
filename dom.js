692b4026e72057f741e42b2cb3c91d234fa16daf
Elyse <elyse-groenendaal@outlook.com>
2022-03-08 10:18:56 +0900

    added dom file and example

---
+++ dom.js
@@ -0,0 +1,16 @@
+// change properties from css class example
+
+/* <style>
+  .square {
+    width: 100px;
+    height: 100px;
+    background-color: crimson;
+  }
+</style>
+
+<div class="red-square"></div> */
+
+// <script>
+const redSquare = document.querySelector(".square");
+redSquare.style.backgroundColor = "limegreen";
+// </script>
